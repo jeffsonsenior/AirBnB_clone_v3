638fe2bf388ab34a9b1230d96729fb3a7b9ea2e5
jeffsonsenior <kinyuajeff@gmail.com>
2024-08-05 16:25:37 +0000

    updatting DB storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,23 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls,id):
+        """retrive an object from the file storage by class and id"""
+        if cls and id:
+            if cls in clases.values() and isinstance(id, str):
+                all_objects = self.all(cls)
+                for key, value in all_objects.items():
+                    if key.split('.')[1] == id:
+                       return value
+            else:
+                return
+        return
+
+    def count(self, cls=None):
+        """count the number of objects in storage matching the given class."""
+        if not cls:
+            inst_of_all_cls = self.all()
+            return len(inst_of_all_cls)
+ 
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,11 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """rerive an object from file storage by class and id."""
+        if class and id:
+
+    def count(self, cls=None):
+        """count the number of object
+
