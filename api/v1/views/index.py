77568197a80f1db302d04b2a024d6917ca2b8347
jeffsonsenior <kinyuajeff@gmail.com>
2024-08-07 02:46:57 +0000

    creating a new viw of place

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,14 +2,30 @@
 """creating flask app and register the blueprint app_views to Flask instance app"""
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
 from flask import jsonify
 
 @app_views.route('/status')
 def api_status():
     """
-    Defining API status
+    Return a Json response for RESTful API health
     """
     response = {'status': "OK"}
     return jsonify(response)
 
+
+@app_views.route('/status')
+def get_stats():
+    """
+
+
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'review': storage.count('Review'),
+        'place': storage.count('Place'),
+        'state': storage.count('State'),
+        'user': storage.count('User'),
+    }
+    return jsonify(stats)
